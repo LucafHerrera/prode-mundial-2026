fff28999d9ee3ba41c6edc9865fe638552c52661
Luca <luca.herrera01@gmail.com>
2026-05-21 09:30:55 -0300

    Configurar anon key

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.ENV = {
-  SUPABASE_URL: "https://hucuakpnbnovftzseekk.supabase.co",
-  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1Y3Vha3BuYm5vdmZ0enNlZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDA1NTIsImV4cCI6MjA5NDg3NjU1Mn0.qgYnXoawJHRVbubVr55roIt9xouNmEV6nAJe-jeHptY"
-};
+  SUPABASE_URL: "https://hucuakpnbnovftzseekk.supabase.co", 
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1Y3Vha3BuYm5vdmZ0enNlZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDA1NTIsImV4cCI6MjA5NDg3NjU1Mn0.qgYnXoawJHRVbubVr55roIt9xouNmEV6nAJe-jeHptYU"
+ };
